c324ec01492736b9bd190afd2fd8c9e6ae1e76a8
Pallavi <pallavipallavisreeja@gmail.com>
2026-04-15 12:45:55 +0530

    Updated a comment

--- routers/candidates.py
+++ routers/candidates.py
@@ -85,3 +85,5 @@ def delete_candidate(candidate_id: int, db: Session = Depends(get_db)):
         crud.delete_candidate(db, candidate_id)
     except crud.CandidateNotFoundError as exc:
         raise HTTPException(status_code=http_status.HTTP_404_NOT_FOUND, detail=str(exc)) from exc
+
+#test non api change
